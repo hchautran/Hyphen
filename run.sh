2b001fb44cf292cb21fce9a1d76b1e797b3983ad
chau <tranhoaichau.00@gmail.com>
2024-06-21 09:52:20 +0700

    update

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
-for model in ssm4rc hyphen
+for model in hyphen
 do
     for embedding_dim in 200 100 50 
     do 
         for dataset in twitter15 twitter16 politifact antivax pheme figlang_twitter rumoureval 
         do
-            for manifold in lorentz 
+            for manifold in poincare euclidean 
             do
                 CUDA_VISIBLE_DEVICES=1 python run.py --enable-log --manifold $manifold --lr 0.001 --dataset $dataset --batch-size 32 --epochs 50 --max-sents 30 --max-coms 30 --max-com-len 20 --max-sent-len 20 --log-path logging/run --model $model --embedding-dim $embedding_dim 
                 CUDA_VISIBLE_DEVICES=1 python run.py --enable-log --manifold $manifold --lr 0.001 --dataset $dataset --batch-size 32 --epochs 50 --max-sents 30 --max-coms 30 --max-com-len 20 --max-sent-len 20 --log-path logging/run --model $model --embedding-dim $embedding_dim --no-fourier 
